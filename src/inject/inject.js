0cee5588434cb3b3cf1fc439c6f346ce6a1b95c5
Andrei Lisnic <andrei.lisnic@gmail.com>
2014-06-15 09:23:52 +0300

    Remove duplicate entries

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -6,15 +6,27 @@
   var deshittify = function () {
     console.log("Not So Shitty 999 loaded.");
 
-    var map = {}, $entries = $('.ads-list-table tr');
+    var map = {},
+        $entries = $('.ads-list-table tr'),
+        duplicates = 0;
 
     $entries.each(function () {
       var $el = $(this),
           title = strip($el.find('h3 a').text()),
-          price = strip($el.find('.ads-list-table-price').text())
+          price = strip($el.find('.ads-list-table-price').text()),
+          key = title + "-" + price;
 
-      console.log(title, price)
+      if (map[key]) {
+        $el.remove();
+        duplicates++;
+      } else {
+        map[key] = true;
+      }
     })
+
+    if ($entries.length) {
+      console.log("NSS: " + duplicates.toString() + " duplicates removed");
+    }
   }
 
   chrome.extension.sendMessage({}, function(response) {
