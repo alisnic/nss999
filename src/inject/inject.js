c37cab1f67b178d95aa6fa86955bd7a74cd60a33
Andrei Lisnic <andrei.lisnic@gmail.com>
2014-06-15 09:31:15 +0300

    Remove entries without price

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,19 +2,27 @@
   "use strict";
 
   var strip = function (s) { return s.replace(/^\s+|\s+$/g, ''); };
+  var nslog = function (s) { console.log("NSS: " + s); }
 
   var deshittify = function () {
     console.log("Not So Shitty 999 loaded.");
 
     var map = {},
         $entries = $('.ads-list-table tr'),
-        duplicates = 0;
+        duplicates = 0,
+        withoutPrice = 0;
 
     $entries.each(function () {
       var $el = $(this),
           title = strip($el.find('h3 a').text()),
           price = strip($el.find('.ads-list-table-price').text()),
           key = title + "-" + price;
+
+      if (price.length === 0) {
+        nslog(title + " - no price");
+        withoutPrice++;
+        return $el.remove();
+      }
 
       if (map[key]) {
         $el.remove();
@@ -25,7 +33,8 @@
     })
 
     if ($entries.length) {
-      console.log("NSS: " + duplicates.toString() + " duplicates removed");
+      nslog(duplicates.toString() + " duplicates removed");
+      nslog(withoutPrice.toString() + " without price removed");
     }
   }
 
