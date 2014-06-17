922281b8795b48e6a83533bac66711830d232088
Andrei Lisnic <andrei.lisnic@gmail.com>
2014-06-17 23:21:35 +0300

    Filter detailed and thumb views

--- js/nss_main.js
+++ js/nss_main.js
@@ -4,31 +4,13 @@
   var strip = function (s) { return s.replace(/^\s+|\s+$/g, ''); };
   var nslog = function (s) { console.log("NSS: " + s); }
 
-  var deshittify = function () {
-    var map = {},
-        $entries = $('.ads-list-table tr'),
-        $objects = $('object'),
-        duplicates = 0,
-        withoutPrice = 0,
-        floatingAds = 0;
+  var dedupe = function ($list, titleSelector, priceSelector) {
+    var map = {}, duplicates = 0, withoutPrice = 0
 
-    $objects.each(function () {
-      var $el = $(this)
-
-      if ($el.parent().css('z-index') === '10000') {
-        $el.parent().remove()
-        floatingAds++
-      }
-    })
-
-    if ($objects.length) {
-      nslog(floatingAds.toString() + " floating ads removed")
-    }
-
-    $entries.each(function () {
+    $list.each(function () {
       var $el = $(this),
-          title = strip($el.find('h3 a').text()),
-          price = strip($el.find('.ads-list-table-price').text()),
+          title = strip($el.find(titleSelector).text()),
+          price = strip($el.find(priceSelector).text()),
           key = title + "-" + price;
 
       if (price.length === 0) {
@@ -44,10 +26,32 @@
       }
     })
 
-    if ($entries.length) {
-      nslog(duplicates.toString() + " duplicates removed");
-      nslog(withoutPrice.toString() + " without price removed");
+    return duplicates
+  }
+
+  var deshittify = function () {
+    var $entries = $('.ads-list-table tr'),
+        $detailedEntries = $('ul.ads-list-detail li'),
+        $photoEntries = $('ul.ads-list-photo li'),
+        $objects = $('object'),
+        floatingAds = 0;
+
+    $objects.each(function () {
+      var $el = $(this)
+
+      if ($el.parent().css('z-index') === '10000') {
+        $el.parent().remove()
+        floatingAds++
+      }
+    })
+
+    if ($objects.length) {
+      nslog(floatingAds.toString() + " floating ads removed")
     }
+
+    console.log(dedupe($entries, 'h3 a', '.ads-list-table-price'))
+    console.log(dedupe($detailedEntries, '.ads-list-detail-item-title a', '.ads-list-detail-item-price'))
+    console.log(dedupe($photoEntries, '.ads-list-photo-item-title a', '.ads-list-photo-item-price'))
   }
 
   $(document).ready(deshittify)
